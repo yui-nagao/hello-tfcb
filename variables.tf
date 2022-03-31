88229970450b0dbf2fc3f0b0ce80393cbf7b6792
yui-nagao <yui.nagao.ym@jalinfotec.co.jp>
2022-03-31 16:04:40 +0900

    modularization

--- variables.tf
+++ variables.tf
@@ -1,19 +1,6 @@
-variable "prefix" {
-  default = "y-nagao"
-}
-variable "address_space" {
-  default = "10.0.0.0/16"
-}
-variable "subnet_prefix" {
-  default = "10.0.10.0/24"
-}
 variable "access_key" {}
 variable "secret_key" {}
-variable "region" {}
-variable "ami" {}
-variable "hello_tf_instance_count" {
-    default = 1
+variable "region" {
+    default = "ap-northeast-1"
 }
-variable "hello_tf_instance_type" {
-    default = "t2.micro"
-}
+
